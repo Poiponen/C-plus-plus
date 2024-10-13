6303650c980ad78bffd9de41a4ee88571bea706e
Poiponen <105865974+Poiponen@users.noreply.github.com>
2024-10-13 14:32:01 +0300

    Create count.cpp

---
+++ count.cpp
@@ -0,0 +1,12 @@
+// Python to C++ translation
+#include <iostream>
+
+int main() {
+    int count = 1; // Initialize count to 1
+    // run loop till count is less than 5
+    while (count < 5) {
+        std::cout << count << std::endl; // Print the current count
+        count = count + 1; // Increment count by 1
+    }
+    return 0;
+}
